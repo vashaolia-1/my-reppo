fb4672088c93bebfa4f6e45d79d90cbe3b8af8b5
OliaVashchenko <gana.olga@gmail.com>
2025-12-25 21:26:06 +0200

    added configs for html

--- lesson21/playwright.config.ts
+++ lesson21/playwright.config.ts
@@ -22,7 +22,7 @@ export default defineConfig({
     /* Opt out of parallel tests on CI. */
     workers: process.env.CI ? 1 : undefined,
     /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-    reporter: [['line'], ['allure-playwright']],
+    reporter: [['line'], ['allure-playwright'], ['html']],
     /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
     use: {
         /* Base URL to use in actions like `await page.goto('')`. */
