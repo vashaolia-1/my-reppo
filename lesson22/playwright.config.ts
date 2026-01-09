b57e530763252b25052f0a3b9d968020de178c87
OliaVashchenko <gana.olga@gmail.com>
2026-01-09 13:22:31 +0200

    yaml edit 12

--- lesson22/playwright.config.ts
+++ lesson22/playwright.config.ts
@@ -38,7 +38,7 @@ export default defineConfig({
             name: 'chromium',
             use: {
                 ...devices['Desktop Chrome'],
-                headless: false,
+                headless: true,
                 viewport: {
                     width: 1680,
                     height: 900
